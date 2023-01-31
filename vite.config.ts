bf1155222682ee99a88cdb2f056b620460b8a735
Paulo Marolla <paulo.marolla@mutual.club>
2023-01-30 21:23:30 -0300

    feat: sentry configuration finished

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig, loadEnv } from 'vite'
 import { sentryVitePlugin } from '@sentry/vite-plugin'
 
 export default ({ mode }) => {
